764b20f68d6746717e0731b650854124f5c8153d
jimmy <jimmy@stenke.org>
2018-10-12 15:25:49 +0700

    fix carousel null pointer

--- app/helpers/spree/carousels_helper.rb
+++ app/helpers/spree/carousels_helper.rb
@@ -2,7 +2,9 @@ module Spree
   module CarouselsHelper
     def render_carousel(name)
       # noinspection RailsChecklist05
-      @carousel = Spree::Carousel.find_by_title name
+      carousel = Spree::Carousel.find_by_title name
+      @carousel = []
+      @carousel = carousel.visible_items unless carousel.nil?
       render partial: "spree/carousels/carousel"
     end
   end
